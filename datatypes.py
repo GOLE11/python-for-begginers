db368afe16bce3851a67ff8c4f72897893faad1f
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-21 11:04:59 +0300

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,51 @@
+#Sting letters and numbers
+#x = "yasin"
+#y = 10
+#mystring = str(x) 
+
+#print(type(mystring))
+#print(x)
+
+#Integar numbers
+#x = 10
+#y = 11
+#myNum = 2.5 
+#myNumInt = int(myNum)
+
+#print(type(myNumInt))
+#print(myNum)
+
+#Float decimal numbers
+#x = 4.5
+#y = 20
+#print(type(x))
+#flt = float(y)
+#print(flt)
+
+#List 
+#x = ["yasin" , "abdirashid" , "warsama"]
+#myItems = (("yasin" , "abdirashid"))
+#myList = list(myItems)
+
+#print(type(myList))
+#print(myList)
+
+#Tuple just like the list
+#x = ("yasin" , "abdirashid")
+#items = ("yasin" , "abdirashid")
+#myTuple = tuple(items)
+#print(type(myTuple))
+
+#Dictionary
+#x = {"name" , "age" , "30"}
+#items = dict(name= "yasin" , age= "24")
+#myDictionary = dict(items)
+#print(type(items))
+
+#Set its basically list without key value
+#item = {"yasin" , "abdi"}
+#mayset = set(item)
+#print(type(item))
+
+#Bool ist inly True or False
+#Casting its changing data types
