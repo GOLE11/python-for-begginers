b07d4febdf7486ac4f6ee4ae4095e42bb334ca8c
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-21 11:08:14 +0300

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,45 @@
+#Set waa unordered,unindexed,unique,and uneditable
+#fruit = {"apple","banana","mango","kiwi"}
+#Accessing
+#for x in fruit:
+    #print(x)
+
+#To check if item exists
+#print("banana" in fruit) 
+
+#To add one thing
+#fruit.add("orange")
+#print(fruit)
+
+#To add multiple items
+#fruit.update(["kiwi","lemon","graps"])
+#print(fruit)
+
+#To remove something out of the set
+#fruit = {"apple","banana","mango","kiwi"}
+#fruit.remove("banana")
+#print(fruit)
+
+#or 
+#fruit.discard("banana")
+#print(fruit)
+
+#Or
+#fruit.pop()
+#print(fruit)
+
+#To clear all set items
+#fruit = {"apple","banana","mango","kiwi"}
+#fruit.clear()
+#print(fruit)
+
+#To combine two sets
+#fruit = {"apple","banana","mango","kiwi"}
+#fruit2 = {"grape","orage"}
+#fruit3 = fruit.union(fruit2)
+
+#print(fruit3)
+
+#Or 
+#fruit.update(fruit2)
+#print(fruit)
