8b382dedcb4eae93bf83e2ac8bb30a5d68d590b2
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-21 11:06:53 +0300

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,62 @@
+#Operators  waa xisaabaad sida + - * / waxana la dhahaa qaybtan Arithmetic
+#Modulus waa hadhaa waana qaybta ugu horeysa operatorska calaamadiisuna waa %
+#example of modulus
+#x = 5
+#y = 2
+#print(x % y)
+
+#Exponentation waa laba jeer ku dhufasho waxa calaamad uah **
+#example
+#x = 5
+#y = 3
+#print(x ** 3)
+
+#Floor waa labajeer qaybin adigoo raba inaad hesho intiger number calaamadiisa //
+#example
+#x = 15
+#y = 2
+#print(x // y)
+
+#Comparison waa isbarbardhig waxa calaamad uah ==  waxaana marwalba kuusoobaxaya True or False
+#marka aanay iskumid ahayn calaamadedu waa !=
+#marka ay kala badanyihiin >
+#marka ay kala yar yihiin <
+#leeg ama ka weyn >=
+#leeg ama kayar <=
+#exaple
+#x = 10
+#y = 9
+#print(x == y)
+#print(x > y)
+#print(x >= y)
+#print(x != y)
+
+#LOGICAL operators waa and, or, not waxayna soosaarayaan true or false
+#example 
+#y = 5 
+#x = 3 
+#t = 10
+#j = 9
+
+#if not y == x and t == j:
+  #print('they are all same')
+
+#else:
+    #print('not all same')
+
+#Identity Operators; waa is, is not waxayna soosaarayaan true or false
+#example
+#x = 5 
+#y = 2
+#print(x is y)
+#print(x is not y)
+
+#Membershib; waa in , not in waxayna soosaari true or false
+# badanaa waxa loo isticmaalaa functions variables and if statements and more others
+#example of variable
+#x = ("canbe" , "moos")
+#y = "moos"
+#print(y in x) 
+#print( y not in x)
+
+#print(x)
