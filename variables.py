de40648292d17fd7c5f55bc311c5e97dc2e0588f
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-21 11:04:12 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+# this is my first code
+#x = 10
+
+#print(x)
+
+#variable
+#x = 5
+#y = 8
+
+#print(x + y)
+#x = 10
+#y = "yass"
+
+#print(x , y)
+#x = "yass"
+#y = " is my nickname"
+
+#print(x + y) 
