ab5115fe6939244f962594e6bc7bf3ab55227aee
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-21 11:07:40 +0300

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,58 @@
+#Tuple
+#waa meel xog lagu urursado
+#lama bedelikaro unchangeable
+#wayna isdabajoogtaa "indexed"
+
+#methods of tuple
+#1-count
+#2-indexed
+
+#siaad usoosaarto methods-kiisa waxaad sameysaa
+#x = (1,2,3)
+#print(dir(x))
+
+#fruit = ("mango","kiwi","apple","lemon")
+#print(fruit[1:3])
+#print(fruit[0])
+
+#if you want to change an item do this
+
+#x = list(fruit)
+#x[0] = "cabage"
+#y = tuple(fruit)
+#print(x)
+
+#Looping
+#for x in fruit:
+    #print(x)
+
+#for index,x in enumerate(fruit):
+    #print(index,x)
+
+#check if item exists in the tuple
+#if "apple" in fruit:
+    #print("yes")
+
+#else:
+    #print("no")
+
+
+#To print only one tuple check you've added comma in it
+#one_fruit = ("apple",)
+#print(type(one_fruit))
+
+#How to delete the whole tuple
+#fruit = ("mango","kiwi","apple","lemon")
+#del fruit
+#print(fruit)
+
+#How to add 2 tuples
+#num1 = (1,2,3,4,5,)
+#num2 = (6,7,8,9)
+#nums =num1+num2
+#print(nums)
+
+#unpacking or assignment 
+#student = ("yasin","math","25")
+#(name,subject,age) = student
+#print(name,subject,age)
