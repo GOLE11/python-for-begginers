07b337a7e22a117c25fefbfb6931cca7f59813c0
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-22 11:44:05 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#A simple calculator made in python using if statement and input.
+#input its were the user can access the project from the terminal, example
+#statement = input("what is in your mind?")
+#print(statement)
+
+#first_number = float(input("Enter your number:"))
+#operator = input("choose the operator:")
+#second_number = float(input("Enter your second_number"))
+
+#if operator == "+":
+  #  print(first_number+second_number)
+#elif operator == "-":
+  #  print(first_number-second_number)
+#elif operator == "/":
+   # print(first_number/second_number)
+#elif operator == "*":
+  #  print(first_number/second_number)
+#else:
+   # print("Invalid operator")    
