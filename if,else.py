131522914d9ace99483b9cef409db156c1dfefa0
YasinAbdirashid <119516700+GOLE11@users.noreply.github.com>
2022-12-22 11:25:16 +0300

    Create if,else.py

---
+++ if,else.py
@@ -0,0 +1,64 @@
+#IF and ELSE statement
+#LOGICAL conditions
+#Equal a == b
+#Not equal a != b
+#Less than a < b
+#less than or equal a < = b
+#greater than a > b
+#greater than or equal a >= b
+#AND OR
+#IS
+#EXAMPLES
+
+#a = 83
+#b = 55
+#if a > b:
+ #   print("a is greater than b")
+#else:
+    #("b is greater than a")
+
+ #or
+#a = 88
+#b = 88
+
+#if a > b:
+ #   print("a is greather than b")
+#elif a == b:
+  #  print("a is equal to b")       
+
+
+#AND, OR statement examples
+
+#a = 55
+#b = 58
+#c = 66
+
+#if a > b and c > a:
+ #   print("a is greather than b and c")
+#else:
+  #  print("b and c are greater than a")
+
+#a = 90
+#b = 58
+#c = 66
+
+#if a > b or c > a:
+  #  print("a is greather than b and c is greather than b")
+#else:
+   # print("b and c are greater than a")    
+
+#IS statement used to evaluate if the IDs are equal or not
+
+#a = [1,2,3,4]
+#b = [1,2,3,4]
+#if we print(a is b) it will be false because the id of these two numbers is different, to know the id print.
+#print(id(a))
+#print(id(b))
+#and then you will see the difference in the terminal
+
+#if you don't want to run the code do this for example
+#a = 2
+#b = 4
+#if a < b:
+    #pass
+#and it will not run anything although you'll not see any error
